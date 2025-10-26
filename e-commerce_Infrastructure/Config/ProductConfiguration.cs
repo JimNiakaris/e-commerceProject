1f220c1c26daf5dceeb96cc40bfeb40d368b0202
dimni <dimni@DESKTOP-BLP3A9Q>
2025-10-26 17:40:27 +0200

    Add project files.

---
+++ e-commerce_Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using e_commerce_Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_Infrastructure.Config
+{
+    // Class to apply Entity configurations intead of applying them in the DbContext
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ e-commerce_Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using e_commerce_Core.Entities;
+using e_commerce_Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce_Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly); //applying the configuratios once in the DbContext here
+        }
+    }
+}
