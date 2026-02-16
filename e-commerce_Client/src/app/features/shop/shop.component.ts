c660e0fa4314eab756729b8ac8c643e0523091f6
jimniakaris <jimniakaris@gmail.com>
2026-02-16 16:29:47 +0200

    shop parameters for sort, brands and types and new pagination

--- e-commerce_Client/src/app/features/shop/shop.component.ts
+++ e-commerce_Client/src/app/features/shop/shop.component.ts
@@ -9,10 +9,13 @@ import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { MatSelectionList, MatActionList, MatListItem, MatListOption, MatSelectionListChange } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopParams';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Pagination } from '../../shared/models/pagination';
 
 @Component({
   selector: 'app-shop',
-  imports: [MatCard, ProductItemComponent, MatButton, MatIcon, MatMenu, MatSelectionList, MatMenuTrigger, MatActionList, MatListOption],
+  imports: [MatCard, ProductItemComponent, MatButton, MatIcon, MatMenu, MatSelectionList, MatMenuTrigger, MatListOption, MatPaginator],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
@@ -20,12 +23,12 @@ export class ShopComponent implements OnInit {
   baseUrl = "https://localhost:7126/api/"
   private shopService = inject(ShopService)
   private dialogeService = inject(MatDialog)
-  products: Product[] = [];
-  selectedBrands: string[] = [];
-  selectedTypes: string[] = [];
-  selectedSort: string = 'name';
+  products?: Pagination<Product>;
+
   sortOptions = [{ name: 'Alphabetical', value: 'name' }, { name: 'Price Low-Hight', value: 'priceAsc' }, { name: 'Price High-Low', value: 'priceDesc' }]
   //products = signal<Product[]>([]); with NO zonechange detection
+  shopParams = new ShopParams();
+  pageSizeOptions = [5,10,15,20]
 
   ngOnInit(): void {
     this.initializeShop();
@@ -39,16 +42,24 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts(this.selectedBrands,this.selectedTypes,this.selectedSort).subscribe({
-      next: response => this.products = response.data, //next: response => this.products.set(response.data), with NO zonechange detection
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => this.products = response, //next: response => this.products.set(response.data), with NO zonechange detection
       error: error => console.log(error)
+    })
+  }
+
+  handlehandlePageEvent(event: PageEvent){
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response=> this.products = response,
+      error: error => console.error(error),
+      
     })
   }
 
   onSortChange(event: MatSelectionListChange) {
     const selectedOption = event.options[0];
     if (selectedOption) {
-      this.selectedSort = selectedOption.value;
+      this.shopParams.sort = selectedOption.value;
       this.getProducts();
     }
   }
@@ -57,16 +68,16 @@ export class ShopComponent implements OnInit {
     const dialogRef = this.dialogeService.open(FiltersDialogComponent, {
       minWidth: '500px',
       data: {
-        selectedBrands: this.selectedBrands,
-        selectedTypes: this.selectedTypes
+        selectedBrands: this.shopParams.brands,
+        selectedTypes: this.shopParams.types
       }
     });
     dialogRef.afterClosed().subscribe({
       next: result => {
         if (result) {
           console.log(result);
-          this.selectedBrands = result.selectedBrands;
-          this.selectedTypes = result.selectedTypes;
+          this.shopParams.brands = result.selectedBrands;
+          this.shopParams.types = result.selectedTypes;
           this.getProducts();
         }
       }
