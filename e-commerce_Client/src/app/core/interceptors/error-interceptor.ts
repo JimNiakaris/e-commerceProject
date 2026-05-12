9b5bad07108e8d9448eeb6db3ce9606bef6e2930
dimni <jimniakaris@gmail.com>
2026-05-12 22:08:59 +0300

    error handler in client plus snackbar material for error display

---
+++ e-commerce_Client/src/app/core/interceptors/error-interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { SnackbarService } from '../service/snackbar.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const snackbar = inject(SnackbarService);
+  
+  return next(req).pipe(
+    catchError((err:HttpErrorResponse)=>{
+      if(err.status === 400){
+        snackbar.error(err.error.title || err.error);
+      }
+
+      if(err.status === 401){
+        snackbar.error(err.error.title || err.error);
+      }
+      if(err.status === 500){
+        router.navigateByUrl('/server-error');
+      }
+      if(err.status === 404){
+        router.navigateByUrl('/not-found');
+      }
+
+      return throwError(()=>err)
+    })
+  )
+};
--- e-commerce_Client/src/app/features/test-error/test-error.component.ts
+++ e-commerce_Client/src/app/features/test-error/test-error.component.ts
@@ -14,14 +14,14 @@ export class TestErrorComponent {
   private http = inject(HttpClient);
 
   get404Error(){
-    this.http.get(this.baseUrl + 'buggy/notfound').subscribe({
+    this.http.get(this.baseUrl + 'error/notfound').subscribe({
       next: response=> console.log(response),
       error: error => console.log(error)
     })
   }
 
    get400Error(){
-    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+    this.http.get(this.baseUrl + 'error/badrequest').subscribe({
       next: response=> console.log(response),
       error: error => console.log(error)
     })
@@ -29,7 +29,7 @@ export class TestErrorComponent {
 
 
  get401Error(){
-    this.http.get(this.baseUrl + 'buggy/unauthorized').subscribe({
+    this.http.get(this.baseUrl + 'error/unauthorized').subscribe({
       next: response=> console.log(response),
       error: error => console.log(error)
     })
@@ -37,14 +37,14 @@ export class TestErrorComponent {
 
 
  get500Error(){
-    this.http.get(this.baseUrl + 'buggy/internalerror').subscribe({
+    this.http.get(this.baseUrl + 'error/internalerror').subscribe({
       next: response=> console.log(response),
       error: error => console.log(error)
     })
   }
 
   get400ValidationError(){
-    this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
+    this.http.post(this.baseUrl + 'error/validationerror', {}).subscribe({
       next: response=> console.log(response),
       error: error => console.log(error)
     })
